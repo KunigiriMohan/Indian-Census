380864ba6e4ef4504d8cac585b99516d19222a01
KunigiMohan <kmohanr5@gmail.com>
2021-11-30 13:37:46 +0530

    Added : Class for Creating Custom Exception.

---
+++ src/main/java/CensusAnalyserException.java
@@ -0,0 +1,14 @@
+
+
+public class CensusAnalyserException extends Exception{
+    public enum ExceptionType {
+        CENSUS_FILE_INCORRECT, CENSUS_INCORRECT_FILE_FORMAT, CENSUS_WRONG_DELIMITER, CENSUS_WRONG_HEADER;
+    }
+
+    public ExceptionType type;
+
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+}
